5abdac8ab57dffbd09e9e7d8293be28b0a20bb13
stenath-tech <stevenath2018@gmail.com>
2023-04-27 10:34:58 +0100

    update

--- shopping/settings.py
+++ shopping/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR/ 'static/'
+STATICFILES_DIR = BASE_DIR / 'home/static'
 # STATIC_ROOT = os.path.join(BASE_DIR/ 'static')
 
 MEDIA_URL = '/media/'
